a46585044ea505f4b3792860d7631d6a26a0a0e5
andrewariley87 <andrewariley87@gmail.com>
2019-12-15 10:55:45 -0500

    add shopping list functionality

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Pounds of Dough', 3),
+    new Ingredient('Bags of Chocolate Chips', 5)
+  ];
   constructor() { }
 
   ngOnInit() {
